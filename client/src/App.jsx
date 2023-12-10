90b85107fec2ef009c9f81f65ad91e3b2e02a3dd
Lukasz <lukasz.wilk231@gmail.com>
2023-12-10 16:28:15 +0100

    fifth-chat-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io.connect("http://localhost:3001");
 
 function App() {
 
@@ -17,14 +17,21 @@ function App() {
          ...prevVal,
          [name]: value,
       }
-    })
-  }
+    });
+  };
+
+  const joinRoom = () => {
+    if(room.userName!=="" && room.roomName!==""){
+      socket.emit("join_room", room);
+    }
+  };
 
   return (
     <div>
       <h1>Join a chat</h1>
       <input onChange={handleChange} type="text" name="userName" id="yourName" placeholder="Your Name" value={room.userName}/>
       <input onChange={handleChange} type="text" name="roomName" id="roomName" placeholder="Room Id" value={room.roomName}/>
+      <button onClick={joinRoom}>Send</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -8,25 +8,25 @@ app.use(cors());
 
 const port = 3001;
 
-const httpServer = http.createServer(app);
-/*
-const io = new Server(httpServer, () => {
-    cors: {
-        origin: "http://localhost:3000",
-        methods:  ["GET", "POST"],
-    },
-})
-*/
+const server = http.createServer(app);
 
-const io = new Server(httpServer);
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ['GET','POST']
+    }
+  })
 
 io.on("connection", socket=>{
     console.log(`User connected on ${socket.id}`)
-    client.on("disconnect", ()=>{
+    socket.on("join_room", arg => {
+        console.log(arg);
+    })
+    socket.on("disconnect", ()=>{
         console.log(`User disconnected on ${socket.id}`)
     })
 })
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`server is running on port ${port}`);;
 })
