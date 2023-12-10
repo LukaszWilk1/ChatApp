faa720bfe481756790b5c16eb3785bd339fe489c
Lukasz <lukasz.wilk231@gmail.com>
2023-12-10 15:55:43 +0100

    fourth-chat-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3001");
+
 function App() {
+
+  const [room, setRoom] = useState({
+    userName: "",
+    roomName: "",
+  });
+
+  const handleChange = e =>{
+    const {name, value} = e.target;
+    setRoom(prevVal => {
+      return {
+         ...prevVal,
+         [name]: value,
+      }
+    })
+  }
+
   return (
     <div>
-      <h1>Hello World!</h1>
-      <h2>Im hungry</h2>
+      <h1>Join a chat</h1>
+      <input onChange={handleChange} type="text" name="userName" id="yourName" placeholder="Your Name" value={room.userName}/>
+      <input onChange={handleChange} type="text" name="roomName" id="roomName" placeholder="Room Id" value={room.roomName}/>
     </div>
   );
 }
