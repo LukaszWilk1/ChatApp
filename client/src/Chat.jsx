60bb7d1bc94db4113e0e97439dcf84b02bc92e68
Lukasz <lukasz.wilk231@gmail.com>
2023-12-12 11:08:31 +0100

    eigth-chat-commit

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from "react";
 const Chat = props =>{
     
     const [currentMessage, setCurrentMessage] = useState("");
+    const [messages, setMessages] = useState([]);
 
     const handleChange = e =>{
         const {value} = e.target;
@@ -15,6 +16,10 @@ const Chat = props =>{
             room: props.propRoom,
         }
 
+        setMessages(prevVal => {
+            return [...prevVal, currentMessage];
+        })
+
         await props.propSocket.emit("send_message", messageData);
     };
 
@@ -22,6 +27,9 @@ const Chat = props =>{
 
         props.propSocket.on("recive_message", data => {
             console.log(data);
+            setMessages(prevVal => {
+                return [...prevVal, data.message];
+            })
         })
 
     }, [props.propSocket])
@@ -31,6 +39,9 @@ const Chat = props =>{
             <h1>User: {props.propUser} has been successfully connected to a room: {props.propRoom}</h1>
             <input type="text" name="yourMessage" id="yourMessage" value={currentMessage} onChange={handleChange}/>
             <button onClick={sendMessage}>Send</button>
+            {messages.map(item => {
+                return item;
+            })}
         </div>
     )
 }
