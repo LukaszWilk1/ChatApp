f38a7e0c0e75d191187904163da8240b026934a7
Lukasz <lukasz.wilk231@gmail.com>
2023-12-13 17:17:52 +0100

    12-chat-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,11 +34,11 @@ function App() {
   return isIn ? (
     <Chat propSocket={socket} propUser = {room.userName} propRoom = {room.roomName}/>
   ) : (
-    <div class="flex flex-col max-w-2xl items-center relative">
-      <h1 class="text-6xl color-lime-300 my-9">JOIN TO THE ROOM</h1>
-      <input onChange={handleChange} type="text" name="userName" id="yourName" placeholder="Your Name" value={room.userName}/>
-      <input onChange={handleChange} type="text" name="roomName" id="roomName" placeholder="Room Id" value={room.roomName}/>
-      <button onClick={joinRoom}>Send</button>
+    <div class="flex flex-col max-w-2xl bg-sky-950 items-center relative my-72 w-fit border border-lime-300 rounded-2xl px-7 py-9">
+      <h1 class="text-6xl color-lime-300 mb-9">JOIN TO THE ROOM</h1>
+      <input class="w-full my-2 text-black px-2 py-1 rounded-lg focus:outline-none" onChange={handleChange} type="text" name="userName" id="yourName" placeholder="Your Name" value={room.userName}/>
+      <input class="w-full my-2 text-black px-2 py-1 rounded-lg focus:outline-none" onChange={handleChange} type="text" name="roomName" id="roomName" placeholder="Room Id" value={room.roomName}/>
+      <button class="mt-9 border border-lime-300 rounded-lg px-4 hover:bg-lime-300 hover:text-sky-900 transition-colors" onClick={joinRoom}>SEND</button>
     </div>
   )
 }
