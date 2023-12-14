387bf87cc04912fdc39c4f129da8e63d0124b5ee
Lukasz <lukasz.wilk231@gmail.com>
2023-12-14 16:14:56 +0100

    16-chat-commit

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -38,14 +38,17 @@ const Chat = props =>{
     }, [props.propSocket]);
 
     return (
-        <div id="Chat" style={{height: "90vh"}} class="w-full flex flex-col max-w-2xl bg-sky-900 items-center relative w-fit rounded-md px-7 py-9 border border-lime-300">
+        <div id="Chat" style={{height: "90vh"}} class="w-96 flex flex-col max-w-2xl bg-sky-900 items-center relative w-fit rounded-md px-2 pt-9 border border-lime-300">
             <h3 class="text-6xl color-lime-300 mb-9 text-center"> CHAT </h3>
-            <input type="text" name="yourMessage" id="yourMessage" value={currentMessage} onChange={handleChange} class="w-full my-2 bg-sky-950 text-lime-300 px-2 py-1 rounded-lg outline-1 focus:outline-lime-300"/>
-            <button class="mt-9 mb-9 border border-lime-300 bg-sky-900 font-bold rounded-lg px-6 py-1.5 hover:bg-lime-300 hover:text-sky-900 transition-colors" onClick={sendMessage}>Send</button>
-            <div class="w-full overflow-auto">
+            <hr class="w-full h-0.5 bg-lime-300 mb-2"/>
+            <div class="w-full h-full overflow-auto">
             {messages.map(item => {
                 return <li>{item.name} : {item.message}</li>
             })}
+            </div>
+            <div class="flex flex-row w-full my-4">
+            <input type="text" name="yourMessage" id="yourMessage" value={currentMessage} onChange={handleChange} class="border border-lime-300 bg-sky-950 text-lime-300 px-2 py-1 rounded-l-lg outline-1 focus:outline-lime-300 h-8 w-full"/>
+            <button class="border-y border-r border-lime-300 bg-sky-900 font-bold rounded-r-lg px-6 hover:bg-lime-300 hover:text-sky-900 transition-colors h-8" onClick={sendMessage}>Send</button>
             </div>
         </div>
     )
