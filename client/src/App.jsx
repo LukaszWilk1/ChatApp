69e344a9e317091fab0a6a2cda281d44a2ba4e00
Lukasz <lukasz.wilk231@gmail.com>
2023-12-14 17:45:38 +0100

    18-chat-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,11 +34,11 @@ function App() {
   return isIn ? (
       <Chat propSocket={socket} propUser = {room.userName} propRoom = {room.roomName}/>
   ) : (
-    <div class="flex flex-col max-w-2xl bg-sky-900 items-center relative w-fit rounded-md px-7 py-9 border border-lime-300">
-      <h1 class="text-6xl color-lime-300 mb-9">JOIN TO THE ROOM</h1>
-      <input class="w-full my-2 bg-sky-950 text-lime-300 px-2 py-1 rounded-lg outline-1 focus:outline-lime-300" onChange={handleChange} type="text" name="userName" id="yourName" placeholder="Your Name" value={room.userName}/>
-      <input class="w-full my-2 bg-sky-950 text-lime-300 px-2 py-1 rounded-lg outline-1 focus:outline-lime-300" onChange={handleChange} type="text" name="roomName" id="roomName" placeholder="Room Id" value={room.roomName}/>
-      <button class="mt-9 border border-lime-300 bg-sky-900 font-bold rounded-lg px-6 py-1.5 hover:bg-lime-300 hover:text-sky-900 transition-colors" onClick={joinRoom}>SEND</button>
+    <div className="flex flex-col max-w-2xl bg-sky-900 items-center relative w-fit rounded-md px-7 py-9 border border-lime-300">
+      <h1 className="text-6xl color-lime-300 mb-9">JOIN TO THE ROOM</h1>
+      <input className="w-full my-2 bg-sky-950 text-lime-300 px-2 py-1 rounded-lg outline-1 focus:outline-lime-300" onChange={handleChange} type="text" name="userName" id="yourName" placeholder="Your Name" value={room.userName}/>
+      <input className="w-full my-2 bg-sky-950 text-lime-300 px-2 py-1 rounded-lg outline-1 focus:outline-lime-300" onChange={handleChange} type="text" name="roomName" id="roomName" placeholder="Room Id" value={room.roomName}/>
+      <button className="mt-9 border border-lime-300 bg-sky-900 font-bold rounded-lg px-6 py-1.5 hover:bg-lime-300 hover:text-sky-900 transition-colors" onClick={joinRoom}>SEND</button>
     </div>
   )
 }
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -17,10 +17,10 @@ const Chat = props =>{
             name: props.propUser,
         }
 
-        await props.propSocket.emit("send_message", messageData);
         if(messageData.message!==""){
+            await props.propSocket.emit("send_message", messageData);
             setMessages(prevVal => {
-                return [...prevVal, messageData];
+                return [messageData,...prevVal];
             })
         }
 
@@ -31,24 +31,25 @@ const Chat = props =>{
 
         props.propSocket.on("recive_message", data => {
             setMessages(prevVal => {
-                return [...prevVal, data];
+                return [data, ...prevVal];
             })
         })
 
     }, [props.propSocket]);
 
     return (
-        <div id="Chat" style={{height: "90vh"}} class="w-96 flex flex-col max-w-2xl bg-sky-900 items-center relative w-fit rounded-md px-2 pt-9 border border-lime-300">
-            <h3 class="text-6xl color-lime-300 mb-9 text-center"> CHAT </h3>
-            <hr class="w-full h-0.5 bg-lime-300 mb-2"/>
-            <div style={{inlineSize: "280px", overflowWrap: "break-word"}} class="w-full h-full overflow-auto">
-            {messages.map(item => {
-                return <li>{item.name} : {item.message}</li>
+        <div id="Chat" style={{height: "90vh"}} className="w-96 flex flex-col max-w-2xl bg-sky-900 items-center relative w-fit rounded-md px-2 pt-9 border border-lime-300">
+            <h3 className="text-6xl color-lime-300 mb-9 text-center"> CHAT </h3>
+            <hr className="w-full h-0.5 bg-lime-300 mb-2"/>
+            <div style={{inlineSize: "280px", overflowWrap: "break-word"}} className="w-full h-full overflow-auto flex flex-col-reverse pr-2">
+            {
+            messages.map((item, index) => {
+                return <li className="bg-sky-950 my-1 rounded-xl py-1 px-1" key={index}>{item.name} : {item.message}</li>
             })}
             </div>
-            <div class="flex flex-row w-full my-4">
-            <input type="text" name="yourMessage" id="yourMessage" value={currentMessage} onChange={handleChange} class="border border-lime-300 bg-sky-950 text-lime-300 px-2 py-1 rounded-l-lg outline-1 focus:outline-lime-300 h-8 w-full"/>
-            <button class="border-y border-r border-lime-300 bg-sky-900 font-bold rounded-r-lg px-6 hover:bg-lime-300 hover:text-sky-900 transition-colors h-8" onClick={sendMessage}>Send</button>
+            <div className="flex flex-row w-full my-4">
+            <input type="text" name="yourMessage" id="yourMessage" value={currentMessage} onChange={handleChange} className="border border-lime-300 bg-sky-950 text-lime-300 px-2 py-1 focus:outline-none rounded-l-lg h-8 w-full"/>
+            <button className="border-y border-r border-lime-300 bg-sky-900 font-bold rounded-r-lg px-6 hover:bg-lime-300 hover:text-sky-900 transition-colors h-8" onClick={sendMessage}>Send</button>
             </div>
         </div>
     )
